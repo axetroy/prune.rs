3e26293f93afab2ef56dd4b2442a82f43c38547d
Axetroy <axetroy.dev@gmail.com>
2021-11-20 12:58:01 +0800

    format

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use async_recursion::async_recursion;
 use clap::{App, Arg};
 use futures::executor;
 use std::env;
@@ -5,7 +6,6 @@ use std::fs;
 use std::io;
 use std::path::Path;
 use std::path::PathBuf;
-use async_recursion::async_recursion;
 
 pub fn absolute_path(path: impl AsRef<Path>) -> io::Result<PathBuf> {
     let path = path.as_ref();
