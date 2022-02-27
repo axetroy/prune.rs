a136d04bb5020d68e9ffd896bf1453026ffb1d1a
Axetroy <axetroy.dev@gmail.com>
2022-02-28 00:03:12 +0800

    refactor: ruler

--- src/main.rs
+++ src/main.rs
@@ -25,12 +25,12 @@ async fn walk<'a>(dir: PathBuf, ruler: &'a Ruler<'a>) {
             let p = path.path();
             let name = p.file_name().unwrap().to_str().unwrap();
 
-            if ruler.ignore.contains(&name) {
+            if ruler.ignore.contains(&name.to_string()) {
                 continue;
             }
 
             if p.is_dir() {
-                if ruler.folder.contains(&name) {
+                if ruler.folder.contains(&name.to_string()) {
                     println!("{}", absolute_path(p).unwrap().to_str().unwrap());
                     if !(*ruler.check_only) {
                         fs::remove_dir_all(path.path().as_path()).unwrap()
@@ -39,7 +39,7 @@ async fn walk<'a>(dir: PathBuf, ruler: &'a Ruler<'a>) {
                 }
 
                 walk(p, ruler).await;
-            } else if ruler.file.contains(&name) {
+            } else if ruler.file.contains(&name.to_string()) {
                 println!("{}", absolute_path(p).unwrap().to_str().unwrap());
                 if !(*ruler.check_only) {
                     fs::remove_file(path.path().as_path()).unwrap()
@@ -51,10 +51,69 @@ async fn walk<'a>(dir: PathBuf, ruler: &'a Ruler<'a>) {
 }
 
 struct Ruler<'a> {
-    ignore: Vec<&'a str>,
-    folder: Vec<&'a str>,
-    file: Vec<&'a str>,
+    ignore: Vec<String>,
+    folder: Vec<String>,
+    file: Vec<String>,
     check_only: &'a bool,
+}
+
+fn parse_rules() -> Ruler<'static> {
+    let mut ruler = Ruler {
+        ignore: vec![],
+        folder: vec![],
+        file: vec![],
+        check_only: &true,
+    };
+
+    let bytes = include_bytes!("../rulers.txt").to_vec();
+
+    let txt = match String::from_utf8(bytes) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    let lines = txt.lines();
+
+    for mut line in lines {
+        line = line.trim();
+
+        if line.len() == 0 {
+            continue;
+        }
+
+        let first = &line.chars().nth(0).unwrap().to_string();
+
+        if first == "#" {
+            continue;
+        }
+
+        let s = line.find(" ").unwrap();
+
+        if s > 1 {
+            panic!("Invalid ruler: {}", line);
+        }
+
+        let pattern = &line.trim_start_matches(first).trim().to_string();
+
+        if first == "I" {
+            ruler.ignore.push(pattern.to_string());
+            continue;
+        }
+
+        if first == "D" {
+            ruler.folder.push(pattern.to_string());
+            continue;
+        }
+
+        if first == "F" {
+            ruler.file.push(pattern.to_string());
+            continue;
+        }
+
+        panic!("Invalid ruler: {}", line);
+    }
+
+    ruler
 }
 
 fn main() {
@@ -73,19 +132,14 @@ fn main() {
         .arg(Arg::new("ROOT").help("prune dir").required(true).index(1))
         .get_matches();
 
-    let mut ruler = Ruler {
-        ignore: vec![".git", ".vscode", ".github", ".idea"],
-        folder: vec!["node_modules", "bowerComponents", ".cache", ".temp"],
-        file: vec![".DS_Store", ".AppleDouble", ".DS_Store"],
-        check_only: &true,
-    };
-
     // You can check the value provided by positional arguments, or option arguments
     let root = if let Some(i) = matches.value_of("ROOT") {
         Path::new(i).to_path_buf()
     } else {
         env::current_dir().unwrap()
     };
+
+    let mut ruler = parse_rules();
 
     // You can check the value provided by positional arguments, or option arguments
     if matches.is_present("remove") {
